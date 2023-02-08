072abc3bcb52b4de42f19a268f6c2512506d69aa
Darwin <114919297+hikaruwheels@users.noreply.github.com>
2023-02-08 13:43:30 +0100

    Create tarea6b.py

---
+++ tarea6b.py
@@ -0,0 +1,7 @@
+# Imports go at the top
+from microbit import *
+import music
+
+
+kirbynotes = ['c5','a4','c5','b4','c5','b4','g4','','a4','e5','d5','c5','b4','c5','b4','g4']
+music.play(kirbynotes)
