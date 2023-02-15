c59d106a012635f17f5be9cd23a91b4887f41f9d
Darwin <114919297+hikaruwheels@users.noreply.github.com>
2023-02-15 13:15:42 +0100

    Create example11.py

---
+++ example11.py
@@ -0,0 +1,10 @@
+# Imports go at the top
+from microbit import *
+import random
+
+while True:
+    if accelerometer.was_gesture('shake'):
+        display.show(random.randint(0,10))
+        sleep (1000)
+    else:
+        display.show(Image.GHOST)
